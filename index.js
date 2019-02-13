a5d2a3a59db83f05812d13bc9b543c87d0efced0
Kevin G Malazarte <kevin.malazarte@gmail.com>
2019-02-13 03:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+
+for (let i = 0; i < musicians.length; i++) {
+  empty.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+
+return empty;
+}
+
+function johnLennonFacts(facts) {
+  var Lennon = [];
+  let i = 0;
+  
+   while (i < facts.length) {
+   Lennon.push(`${facts[i]}!!!`);
+   i++;
+   }
+   return Lennon;
+}
+
+function iLoveTheBeatles(number) {
+  var Love = [];
+
+do {
+  Love.push("I love the Beatles!");
+  number++;
+} while (number < 15);
+return Love;
+}
+
+
